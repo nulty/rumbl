7d7b75ba86671afa5c62a47287e78056e748d0e0
Iain McNulty <iainmcnulty@gmail.com>
2018-04-12 16:24:25 +0100

    Final config rumbrella

--- apps/rumbl/mix.exs
+++ apps/rumbl/mix.exs
@@ -19,7 +19,7 @@ defmodule Rumbl.Mixfile do
   def application do
     [mod: {Rumbl, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex, :comeonin]]
+                    :phoenix_ecto, :postgrex, :comeonin, :info_sys]]
   end
 
   # Specifies which paths to compile per environment.
@@ -38,7 +38,8 @@ defmodule Rumbl.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:comeonin, "~> 2.0"}]
+     {:comeonin, "~> 2.0"},
+     {:info_sys, in_umbrella: true}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
